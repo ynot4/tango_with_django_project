7367a1375880ad543f072f4d284ec4d4eacfe9d2
Tony Soong <2764149s@student.gla.ac.uk>
2024-01-21 20:41:14 +0000

    Chapter 3 - create rango app

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rango import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("rango/", include("rango.urls")),
+    # The above maps any URLs starting with rango/ to be handled by rango.
+    path("admin/", admin.site.urls),
 ]
