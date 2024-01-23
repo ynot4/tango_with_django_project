9d4f8bd5aba2f2a2dac3716ab6673bf5ca7129de
Tony Soong <2764149s@student.gla.ac.uk>
2024-01-23 00:27:09 +0000

    Setup media directory

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -67,6 +68,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -127,3 +129,6 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
