6c24a91bfb96fbef3a093544ca9fc6cf20bcd6eb
Tony Soong <2764149s@student.gla.ac.uk>
2024-01-23 01:02:11 +0000

    Complete chapter 4

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,5 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse(
-        "Rango says here is the about page. <a href='/rango/'>Index</a>"
-    )
+    context_dict = {"name": "Tony Soong"}
+    return render(request, "rango/about.html", context=context_dict)
